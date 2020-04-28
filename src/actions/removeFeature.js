1e713ab7bba4b54c1132165141feb4ad2a2e3f70
Altyn Berdiyeva <aberdiyeva@lisaacademy.org>
2020-04-28 18:28:54 -0500

    somebroken code is here

---
+++ src/actions/removeFeature.js
@@ -0,0 +1,5 @@
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
+
+export function REMOVE_FEATURE(){
+    return{type: REMOVE_FEATURE}
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./reducers/";
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+// wrap to the App with <provider store=(store)> <provider>
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(<App />, rootElement);
+
+const store = createStore(rootReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
